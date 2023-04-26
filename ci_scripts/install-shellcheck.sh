553714c117297cf06501a4e184757ff6ef4730e0
MohammadReza Palide <mrpalide@gmail.com>
2023-04-26 10:56:52 +0330

    back to stable

--- ci_scripts/install-shellcheck.sh
+++ ci_scripts/install-shellcheck.sh
@@ -5,9 +5,9 @@ osarch="$(uname -m)"
 
 mkdir -p ./scheck
 
-curl -L -o shellcheck-stable.tar.xz "https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.${osname}.${osarch}.tar.xz"
+curl -L -o shellcheck-stable.tar.xz "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.${osname}.${osarch}.tar.xz"
 
 tar -xvf shellcheck-stable.tar.xz -C ./scheck
 
-mv ./scheck/shellcheck-v0.9.0/shellcheck ./shellcheck
+mv ./scheck/shellcheck-stable/shellcheck ./shellcheck
 rm -rf ./scheck ./shellcheck-stable.tar.xz
