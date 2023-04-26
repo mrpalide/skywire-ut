2a648612780d7a92400689f4b2e9f1ab1cb89eee
MohammadReza Palide <mrpalide@gmail.com>
2023-04-26 09:50:37 +0330

    go mod tidy | go mod vendor

--- vendor/github.com/skycoin/skywire/internal/vpn/const.go
+++
@@ -1,9 +0,0 @@
-// Package vpn internal/vpn/const.go
-package vpn
-
-const (
-	// TUNNetmaskCIDR is a general netmask used for all TUN interfaces in CIDR format (only suffix).
-	TUNNetmaskCIDR = "/29"
-	// TUNMTU is MTU value used for all TUN interfaces.
-	TUNMTU = 1500
-)
--- vendor/github.com/skycoin/skywire/internal/vpn/server_config.go
+++
@@ -1,9 +0,0 @@
-// Package vpn internal/vpn/server_config.go
-package vpn
-
-// ServerConfig is a configuration for VPN server.
-type ServerConfig struct {
-	Passcode         string
-	Secure           bool
-	NetworkInterface string
-}
